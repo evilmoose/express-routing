f4e5f973680f7d1052eaa60560a3c4dd0cab0fd2
Evilmoose <ronnielopez@gmail.com>
2023-05-11 14:13:36 -0700

    Added ErrorHandler.js

--- Utilities/util.js
+++ Utilities/util.js
@@ -1,1 +1,12 @@
 // util.js
+// Error Handler
+class ErrorHandler extends Error {
+    constructor(message, status) {
+        super();
+        this.message = message;
+        this.status = status;
+        console.error(this.stack);
+    }
+}
+
+export default ErrorHandler;
