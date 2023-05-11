85117c98dad03b9c10da4973bdb1fbd95bbb36e0
Evilmoose <ronnielopez@gmail.com>
2023-05-11 15:31:55 -0700

    Implemented 'ErrorHandler' to 'app.js'

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 // app.js
 import express from 'express';
+import ErrorHandler from './Utilities/util.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('A route');
+app.get('/', (req, res, next) => {
+    res.send('');
+})
+
+app.get('/mean', (req, res, next) => {
+    res.send('mean');
+})
+
+app.get('/median', (req, res) => {
+    res.send('median');
+})
+
+app.get('/mode', (req, res) => {
+    res.send('mode');
+})
+
+app.get('/all', (req, res) => {
+    res.send('mode');
+})
+
+// ErrorHandler
+app.use((req, res, next) => {
+    const err  = new ErrorHandler("Not found", 404);
+    return next(err);
 })
 
 app.listen(5000, () => {
