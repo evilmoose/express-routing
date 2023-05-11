b00a0647a61a9d098e494723dcb9cf557db0eeb2
Evilmoose <ronnielopez@gmail.com>
2023-05-11 14:33:04 -0700

    Server setup and first route.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // app.js
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('A route');
+})
+
+app.listen(5000, () => {
+    console.log('Listening... Port: 5000')
+})
